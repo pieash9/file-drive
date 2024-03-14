99b78fb3e7561a62fcf987c758b02678401ce424
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-03-14 23:21:27 +0600

    created joined multiple organization functionality

--- convex/files.ts
+++ convex/files.ts
@@ -1,20 +1,38 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const createFile = mutation({
   args: {
     name: v.string(),
+    orgId: v.string(),
   },
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    console.log(identity);
+    if (!identity) {
+      throw new ConvexError("You must be logged in to upload a file!");
+    }
+
     await ctx.db.insert("files", {
       name: args.name,
+      orgId: args.orgId,
     });
   },
 });
 
 export const getFiles = query({
-  args: {},
+  args: {
+    orgId: v.string(),
+  },
   async handler(ctx, args) {
-    return ctx.db.query("files").collect();
+    const identity = await ctx.auth.getUserIdentity();
+    console.log(identity);
+    if (!identity) {
+      return [];
+    }
+    return ctx.db
+      .query("files")
+      .withIndex("by_orgID", (q) => q.eq("orgId", args.orgId))
+      .collect();
   },
 });
---
+++ src/app/header.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import {
+  OrganizationSwitcher,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+const Header = () => {
+  return (
+    <div className="border-b py-4 bg-gray-50">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          File<span className="text-blue-500">Drive</span>
+        </div>
+        <div className="flex gap-2">
+          <OrganizationSwitcher />
+          <UserButton />
+          <SignOutButton>
+            <SignInButton>
+              <Button>Sing In</Button>
+            </SignInButton>
+          </SignOutButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
